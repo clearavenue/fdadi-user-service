15cd7bc860f44704bb716fae0ab3667e4b74c9f7
William Hunt <bill.hunt@clearavenue.com>
2020-09-14 16:04:26 -0400

    Latest spring-boot version and local options

--- src/main/java/com/clearavenue/fdadi/FdadiUserServiceApplication.java
+++ src/main/java/com/clearavenue/fdadi/FdadiUserServiceApplication.java
@@ -2,27 +2,34 @@ package com.clearavenue.fdadi;
 
 import javax.annotation.PostConstruct;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.info.BuildProperties;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
+@RequiredArgsConstructor
 @Slf4j
 public class FdadiUserServiceApplication {
 
-	@Autowired
-	private BuildProperties buildProperties;
+	private final BuildProperties buildProperties;
+
+	@Bean
+	public ApplicationRunner osLogger(Environment environment) {
+		return (arguments) -> {
+			log.info("Starting {} : {}", buildProperties.getName(), buildProperties.getVersion());
+			log.info("Running on {} {} ({})", environment.getProperty("os.name"), environment.getProperty("os.version"), environment.getProperty("os.arch"));
+                        log.info("Startup complete.");
+		};
+	}
 
 	public static void main(final String[] args) {
 		SpringApplication.run(FdadiUserServiceApplication.class, args);
 	}
-
-	@PostConstruct
-	void displayVersion() {
-		log.debug("Starting {} : {}", buildProperties.getName(), buildProperties.getVersion());
-	}
-
 }
