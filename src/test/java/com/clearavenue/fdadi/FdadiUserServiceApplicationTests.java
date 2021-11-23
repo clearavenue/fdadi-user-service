cbc8c99ba5fbe281a0b4c8b80d04ba1ef4b9b076
William Hunt <bill.hunt@clearavenue.com>
2021-11-23 10:38:17 -0500

    hoping for h2 on main app test

--- src/test/java/com/clearavenue/fdadi/FdadiUserServiceApplicationTests.java
+++ src/test/java/com/clearavenue/fdadi/FdadiUserServiceApplicationTests.java
@@ -1,11 +1,19 @@
 package com.clearavenue.fdadi;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.core.AutoConfigureCache;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@AutoConfigureCache
+@AutoConfigureDataJpa
+@AutoConfigureTestDatabase
+@AutoConfigureTestEntityManager
 class FdadiUserServiceApplicationTests {
 
 	@Test
